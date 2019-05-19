532eca0a3329ea39c9f7359fcf04524bebe712c6
smit <smitpatel0045@gmail.com>
2019-05-20 02:46:50 +0530

    updated credentials

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "eu-cdbr-west-02.cleardb.net";
-$user = "b54aaecce86120";
-$pass = "3cc79960";
-$database = "heroku_046e32e37f7ded0";
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "chatbox";
 $conn = mysqli_connect($server,$user,$pass,$database);
 
 if(!$conn){

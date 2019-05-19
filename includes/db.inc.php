cbaef6cae3b1e5e9b3dd3a6e5842db0eb845560a
smit <smitpatel0045@gmail.com>
2019-05-20 02:28:24 +0530

    updated some vars

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "chatbox";
+$server = "eu-cdbr-west-02.cleardb.net";
+$user = "b54aaecce86120";
+$pass = "3cc79960";
+$database = "heroku_046e32e37f7ded0";
 $conn = mysqli_connect($server,$user,$pass,$database);
 
 if(!$conn){
